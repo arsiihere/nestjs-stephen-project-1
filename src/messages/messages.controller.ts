0b38cc040295f7fd89ff82a12603e3a129102305
arsiihere <arsiiahmi@gmail.com>
2022-08-16 04:24:36 +0500

    add the dto's

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from '../dtos/create-message.dto';
+
+@Controller('/messages')
+export class MessagesController {
+  @Get()
+  listMessages() {}
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
+  @Get('/:id')
+  getMessage(@Param() id: string) {
+    console.log(id);
+  }
+}
